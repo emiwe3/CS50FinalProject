8747532b9e46188bfb605b7dd11676ef3b07c098
mkucuk-commits <mkucuk@college.harvard.edu>
2025-12-07 21:17:03 -0500

    Update swirl.js

--- docs/swirl.js
+++ docs/swirl.js
@@ -1,10 +1,13 @@
+// tracks position of mouse 
 let lastX = null;
 let lastY = null;
 let lastAngle = null;
 let swirlScore = 0;
 
+// threshould value for the entrance
 const swirlThreshold = 3; 
 
+// listens for mouse movements
 document.addEventListener("mousemove", (e) => {
   if (lastX === null) {
     lastX = e.clientX;
@@ -12,9 +15,11 @@ document.addEventListener("mousemove", (e) => {
     return;
   }
 
+  // calculates difference in position from previous mouse movement
   const dx = e.clientX - lastX;
   const dy = e.clientY - lastY;
 
+  // calculates the angle of mouse 
   const angle = Math.atan2(dy, dx);
 
   if (lastAngle !== null) {
@@ -23,11 +28,13 @@ document.addEventListener("mousemove", (e) => {
     if (delta > Math.PI) delta -= 2 * Math.PI;
     if (delta < -Math.PI) delta += 2 * Math.PI;
 
+    // if the mouse angle difference is greate than 0.5 then it is a swirl
     if (Math.abs(delta) > 0.5) {
       swirlScore += 1;
     }
   }
 
+  // updates the last recorded mouse position
   lastAngle = angle;
   lastX = e.clientX;
   lastY = e.clientY;
@@ -37,6 +44,7 @@ document.addEventListener("mousemove", (e) => {
   }
 });
 
+// triggers the effect for the entrance
 function triggerEntrance() {
   document.body.classList.add("fade-out");
 
