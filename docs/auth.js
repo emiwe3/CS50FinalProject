b081fb9abe5ec86788288473b677765aa98f3fdb
mkucuk-commits <mkucuk@college.harvard.edu>
2025-12-07 20:28:21 -0500

    Update auth.js

--- docs/auth.js
+++ docs/auth.js
@@ -7,13 +7,17 @@ import {
   setDoc
 } from "./firebase.js";
 
+// wait for the DOM to fully load
 document.addEventListener("DOMContentLoaded", () => {
+  // register form
   const registerForm = document.getElementById("register-form");
   if (registerForm) registerForm.addEventListener("submit", handleRegister);
 
+  // login form
   const loginForm = document.getElementById("login-form");
   if (loginForm) loginForm.addEventListener("submit", handleLogin);
 
+  // redirect to register page
   const goRegister = document.getElementById("go-register");
   if (goRegister) {
     goRegister.addEventListener("click", (e) => {
@@ -21,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "register.html";
     });
   }
-
+// redirect to login page
   const goLogin = document.querySelector('a[href="login.html"]');
   if (goLogin) {
     goLogin.addEventListener("click", (e) => {
@@ -31,26 +35,32 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
 async function handleRegister(e) {
   e.preventDefault();
 
+  // get user info
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value;
   const confirm = document.getElementById("confirm").value;
 
+  // confirm that all fields are filled out
   if (!email || !password || !confirm) {
     alert("Please fill all fields.");
     return;
   }
+   // confirm that password matches
   if (password !== confirm) {
     alert("Passwords don't match.");
     return;
   }
+  // confirm that password is 6 or more characters
   if (password.length < 6) {
     alert("Password must be at least 6 characters.");
     return;
   }
 
+  // use firebase for authentication
   try {
     const userCred = await createUserWithEmailAndPassword(auth, email, password);
     const uid = userCred.user.uid;
@@ -67,6 +77,7 @@ async function handleRegister(e) {
     e.target.reset();
     window.location.href = "login.html";
 
+  // handle registration errors
   } catch (err) {
     if (err.code === "auth/email-already-in-use") {
       alert("Email already registered.");
